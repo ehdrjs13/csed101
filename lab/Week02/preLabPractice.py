98f5c4b3d5456bb706567b24c7b2079728b2725a
Donggeon Kim <128569780+ehdrjs13@users.noreply.github.com>
2026-03-06 17:20:26 +0900

    setup

---
+++ lab/Week02/preLabPractice.py
@@ -0,0 +1,10 @@
+def transform(f):
+    result = (float(f)-32)*5/9
+    
+    return result
+
+temp = input("Enter the temperature in Fahrenheit:")
+
+result = transform(temp)
+
+print('Celsius:',result)
