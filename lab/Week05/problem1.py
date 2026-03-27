6d5228d4e8c9f093d02aa11a7ed0ccb242ea95bc
Donggeon Kim <128569780+ehdrjs13@users.noreply.github.com>
2026-03-27 21:40:26 +0900

    merge problem1

---
+++ lab/Week05/problem1.py
@@ -0,0 +1,10 @@
+def mulTable() -> None:
+    for y in range(1,10):
+        for x in range(1,10):
+            print(f"{x} * {y} = {x*y}", end="\t")
+        print()
+        
+
+    return
+
+mulTable()
